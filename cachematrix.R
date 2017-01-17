636c540ca1befa8dfecec42fa541c19379b1a6e6
AluruRavindraBabu <AluruRavindraBabu@users.noreply.github.com>
2017-01-17 21:28:34 +0100

    Update program as per assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+## Computing the inverse of a square matrix can be done with the solve function in R. 
+## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+                  init.mat <- NULL     ### Initializing matrix with NULL
+                      set <- function(y) {
+                        x <<- y
+                        init.mat <<- NULL
+                      }
+  
+                      get <- function() x
+                      setinv <- function(matinverse) init.mat <<- matinverse
+                      getinv <- function() init.mat
+                      list(set = set, get = get, setinv = setinv, getinv = getinv)              
+                      
 }
 
 
-## Write a short comment describing this function
 
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
+## Return a matrix that is the inverse of "x"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  init.mat <- x$getinv()
+  if(!is.null(init.mat)) {
+    print("cached data")
+    return(init.mat)
+  }
+  data <- x$get()
+  init.mat <- solve(data, ...)
+  x$setinv(init.mat)
+  init.mat
 }
